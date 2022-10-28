042aaadaf38f6bb56deb3a0b2c8d2cead8469d48
emily <emily@bacheconsulting.com>
2022-10-28 17:04:21 +0200

    tweaks

--- csharp/LockController/LockControllerLib/IMassiveDbConnection.cs
+++ csharp/LockController/LockControllerLib/IMassiveDbConnection.cs
@@ -5,7 +5,7 @@ public interface IMassiveDbConnection
     IMassiveSession Session { get; set; }
     MassiveLocation getPathTo(string mObject);
     IMassiveObject FullObject(IMassiveLocation path);
-    IMassiveLocation Wangle(IMassiveObject fullMObject);
+    MassiveLocation Wangle(IMassiveObject fullMObject);
 
     int CallMassiveMethod(
                 IMassiveLocation path, 
--- csharp/LockController/LockControllerLib/IMassiveObject.cs
+++ csharp/LockController/LockControllerLib/IMassiveObject.cs
@@ -1,9 +1,18 @@
+using System.Data.Common;
+
 namespace LockControllerLib;
 
 public interface IMassiveObject
 {
+    string Id { get; }
 }
 
 class MassiveObject : IMassiveObject
 {
+    public MassiveObject(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; }
 }
